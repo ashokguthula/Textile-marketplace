c7b6fb8d9cece6370b2ea2341b2ec7be8d279c98
ashokguthula <ashokguttula2@gmail.com>
2026-08-02 22:08:07 +0530

    Setup Express server and project foundation

---
+++ server/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "Marketplace API is running 🚀"
+    });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
